8cb4e720594af66116c9bdd9a8301b469489e45e
William Koller <williamkoller404@gmail.com>
2023-01-25 01:08:13 -0300

    feat: :sparkles: add swagger and set configs

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { swaggerConfig } from './docs/swagger.config';
 
 async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  swaggerConfig(app);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
